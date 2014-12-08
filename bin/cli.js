1e67c76e754e1a0d2a46849c1d1605bb1a16372b
orangemug <info@orangemug.co.uk>
2014-12-08 20:04:02 +0000

    Added cli opts.

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,27 @@
 #!/usr/bin/env node
+var yargs  = require('yargs');
 var fs     = require('fs');
 var byline = require('byline');
 var open   = require('open');
+
+var argv = yargs
+  .describe('n', 'number of lines to process before stdout watch ends')
+  .default('n', 100)
+  .describe('t', 'timeout until parse watch ends')
+  .default('t', 2*1000)
+  .argv;
+
+if(argv.help) {
+  yargs.showHelp();
+  process.exit(1);
+}
 
 // Be invisible...
 process.stdin.pipe(process.stdout);
 
 var stream = byline(process.stdin);
 
-var idx = 0;
+var idx = 1;
 function hdl(line) {
   if(line.toString().match(/<(https?:\/\/[^>]+)>/)) {
     var url = RegExp.$1;
@@ -16,7 +29,7 @@ function hdl(line) {
     close();
   }
 
-  if(idx >= 100) {
+  if(idx >= argv.n) {
     close();
   }
 
@@ -27,5 +40,5 @@ function close() {
   process.stdin.unpipe(stream);
 }
 
-setTimeout(close, 2*1000);
+setTimeout(close, argv.t);
 stream.on('data', hdl);

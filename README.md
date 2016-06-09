2ae7425b8f4a30e05dcde2f282bdf4bd4786984c
orangemug <info@orangemug.co.uk>
2016-06-09 08:43:10 +0100

    Added new usage

--- README.md
+++ README.md
@@ -1,30 +1,41 @@
-# gourl
+# openurl
 Open a url from a processes output, by default it'll wait for the first 100 lines or 2 seconds.
 
-    some-process | gourl
+Basically it'll open anything from stdout of the format `<http://example.com>`.
 
-Basically it'll open anything that in the stdout of the format `<http://example.com>`
+
+## Usage
+Just pipe something to it
+
+    some-process | openurl
+
 
 
 ## Install
 
-    npm i -g git://github.com/orangemug/gourl.git
+    npm install -g orangemug/openurl
 
 
 ## Usage
 Simple usage
 
-    some-process | gourl
+    some-process | openurl
 
 Optional args
 
- * `-n` number of lines to parse before bailing out
- * `-t` time in ms before bailing out
+ * `-n --number` number of lines to parse before bailing out
+ * `-t --timeout` time in ms before bailing out
+ * `-m --multiple [number]` open multiple urls (defaults to 1)
+ * `-i --interval [interval]` used with multiple as the interval in ms between open calls (defaults to `1000`)
 
 So this first example is as same as
 
-    some-process | gourl -n 100 -t 2000
+    some-process | openurl -n 100 -t 2000
+
+If you have a markdown file with a load of urls you want to open, the following will open them one every second (in order to not hammer the poor website)
+
+    cat urls.md | openurl -m 20 -i 1000
 
 
 ## License
-MIT
+[MIT](LICENSE)
--- bin/cli.js
+++ bin/cli.js
@@ -5,15 +5,28 @@ var byline = require('byline');
 var open   = require('open');
 
 var argv = yargs
-  .describe('n', 'number of lines to process before stdout watch ends')
-  .default('n', 100)
-  .describe('t', 'timeout until parse watch ends')
-  .default('t', 2*1000)
+  .describe('number', 'number of lines to process before stdout watch ends')
+  .default('number', 100)
+  .describe('timeout', 'timeout until parse watch ends')
+  .default('timeout', 2*1000)
+  .describe('multiple', 'open multiple urls')
+  .default('multiple', 1)
+  .describe('interval', 'used with multiple as the interval in ms between open calls')
+  .default('interval', 1000)
+  .number("interval")
+  .alias("n", "number")
+  .alias("t", "timeout")
+  .alias("m", "multiple")
+  .alias("i", "interval")
   .argv;
 
 if(argv.help) {
   yargs.showHelp();
   process.exit(1);
+}
+
+if(Number.isNaN(argv.interval)) {
+  argv.interval = argv.i = 1000;
 }
 
 // Be invisible...
@@ -22,14 +35,36 @@ process.stdin.pipe(process.stdout);
 var stream = byline(process.stdin);
 
 var idx = 1;
+var opensLeft = parseInt(argv.multiple, 10);
+var currInterval = 0;
+
 function hdl(line) {
-  if(line.toString().match(/<(https?:\/\/[^>]+)>/)) {
-    var url = RegExp.$1;
-    open(url);
+  var urlRegExp = "<(https?:\/\/[^>]+)>";
+  var matches = line.toString().match(new RegExp(urlRegExp, "g"));
+
+  if(opensLeft < 1) {
     close();
+    return;
   }
 
-  if(idx >= argv.n) {
+  if(matches) {
+    for(var i=0; i<matches.length; i++) {
+      matches[i].match(urlRegExp);
+      var url = RegExp.$1;
+
+      setTimeout(open.bind(this, url), currInterval);
+
+      opensLeft--;
+      currInterval += argv.interval;
+
+      if(opensLeft < 1) {
+        close();
+        break;
+      }
+    }
+  }
+
+  if(idx >= argv.number) {
     close();
   }
 
@@ -40,5 +75,5 @@ function close() {
   process.stdin.unpipe(stream);
 }
 
-setTimeout(close, argv.t);
+setTimeout(close, argv.timeout);
 stream.on('data', hdl);
